80eb464824609d224a3f152674250f73911001af
Adam Stokes <adam.stokes@ubuntu.com>
2016-01-09 23:22:10 +0000

    update copyright header
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- placement/log.py
+++ placement/log.py
@@ -1,7 +1,4 @@
-#
-# log.py - Logger
-#
-# Copyright 2014 Canonical, Ltd.
+# Copyright 2014-2016 Canonical, Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
