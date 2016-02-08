1ad85cc94e90f2ff45ae2ce0f80eb79eda10f386
Michael McCracken <mike.mccracken@canonical.com>
2016-02-08 12:12:16 -0800

    don't move focus to empty actions list
    if there's no selected charm, the actions list will be empty so
    selecting a machine will cause the focus to appear to disappear. let's
    avoid that.
    
    Signed-off-by: Michael McCracken <mike.mccracken@canonical.com>

--- bundleplacer/placerview.py
+++ bundleplacer/placerview.py
@@ -83,7 +83,8 @@ class PlacerView(WidgetWrap):
             self._selected_machines.remove(m)
         else:
             self._selected_machines.add(m)
-        self.pv.focus_actions_column()
+        if len(self._selected_charms) > 0:
+            self.pv.focus_actions_column()
 
     @property
     def selected_machines(self):
